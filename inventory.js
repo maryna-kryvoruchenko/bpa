abe79ddb88d30a420cbead0d514cab6ec45c31a1
kbellinger <buc18003@byui.edu>
2022-12-31 17:51:58 -0700

    work so far on invent page still working on filter

--- inventory.js
+++ inventory.js
@@ -1,26 +1,62 @@
 //INVENTORY PAGE JS
 //bringing json file information into the DOM
 const invGrid = document.querySelector('.inventory-grid');
+const requestURL = 'https://maryna-kryvoruchenko.github.io/bpa/car-info.json';
+let searchBtn = document.querySelector('#topSearchBtn');
+searchBtn.addEventListener('onclick', function(){
+    fetch(requestURL)
+    .then (function (response) {
+        return response.json();
+    })
+    .then(function(jsonObject){
+        const carsObj = jsonObject['cars'];
+        console.log(carsObj);
+        searchBy(carsObj);
+    });
+})
 
-function displayInvItem(cars){
-  console.log('made it to the function')
-  console.log(cars);
-  document.querySelector('.inventory-grid').innerHTML = `<section class="inv-item">
-  <img class="inv-image" src="${cars.image}" alt="${cars.year} ${cars.make} ${cars.model}">
-  <h4>${cars.year} ${cars.make} ${cars.model}</h4>
+
+fetch(requestURL)
+.then (function (response) {
+    return response.json();
+})
+.then(function(jsonObject){
+    const carsObj = jsonObject['cars'];
+    carsObj.forEach(displayInvItem);
+    console.log(carsObj);
+});
+
+function displayInvItem(car){
+//   console.log(car);
+  let template = `<section class="inv-item">
+  <img class="inv-image" src="${car.image}" alt="${car.year} ${car.make} ${car.model}">
+  <h4>${car.year} ${car.make} ${car.model}</h4>
   <ul class="details">
-    <li>Color: ${cars.color}</li>
-    <li>${cars.details}</li>
+    <li>Color: ${car.color}</li>
+    <li>${car.details}</li>
   </ul>
-  <p class="prices">Purchase Price: ${cars.buy} <span> Rental Price: ${cars.rent}</span></p>
-  </section>`;
-
+  <p class="prices">Purchase Price: ${car.buy} <span> Rental Price: ${car.rent}</span></p>
+  </section>`
+  document.querySelector('.inventory-grid').innerHTML += template;
 }
 
-displayInvItem();
+function searchBy(cars){
+    let inpMake = document.querySelector('#make').value;
+    console.log(inpMake);
+        var outMake = cars.filter(car=> car.make === inpMake);
+        console.log(outMake);
+   
 
-fetch('car-info.json')
-.then(function(jsonObject){
-  const car = jsonObject['cars'];
-  car.forEach(displayInvItem);
-})
+    
+    // var result = cars.filter(car=> car.color === "Blue");
+}
+
+
+
+// document.getElementById('topSearchBtn').addEventListener('click', searchBy);
+
+
+
+
+// console.log(result);
+// displayInvItem(cars);
