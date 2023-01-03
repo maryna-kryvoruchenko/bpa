a60626825a5013484bd5439df590a411a3975712
maryna-kryvoruchenko <kry20001@byui.edu>
2023-01-03 14:29:46 -0700

    home progress

--- main.js
+++ main.js
@@ -1,48 +1,8 @@
-//Picture Slideshow
-var currentSlide = 0;
-const slides = document.querySelectorAll(".slide")
-const dots = document.querySelectorAll('.dot')
-
-const init = (n) => {
-  slides.forEach((slide) => {
-    slide.style.display = "none"
-    dots.forEach((dot) => {
-      dot.classList.remove("active")
-    })
-  })
-  slides[n].style.display = "block"
-  dots[n].classList.add("active")
-}
-document.addEventListener("DOMContentLoaded", init(currentSlide))
-
-const next = () => {
-    currentSlide >= slides.length - 1 ? currentSlide = 0 : currentSlide++
-    init(currentSlide)
-  }
-  
-  const prev = () => {
-    currentSlide <= 0 ? currentSlide = slides.length - 1 : currentSlide--
-    init(currentSlide)
-  }
-  
-  document.querySelector(".next").addEventListener('click', next)
-  document.querySelector(".prev").addEventListener('click', prev)
-
-setInterval(() => {
-    next()
-  }, 5000);
-
-dots.forEach((dot, index) => {
-    dot.addEventListener("click", () => {
-    init(index)
-    currentSlide = i
-    })
-  })
 //animated title
 if (document.getElementById("slider--text")) {
     let slides = [
         "<h1>Welcome to Old Bob's Classic Auto</h1>",
-        "<img class='home-logo' src='images/homeLogo.png' alt='logo'>"
+        "<img class='home-logo' src='images/trans-logo.png' alt='logo'>"
     ];
 
     let i = 0;
